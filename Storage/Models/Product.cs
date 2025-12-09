1350175d7a8e261e0d3d458481071d730e4d638b
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-09 15:52:04 +0100

    add Product model

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}

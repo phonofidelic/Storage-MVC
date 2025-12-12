7f9f8238080377116d89a7342552cc22063c1401
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-12 19:10:35 +0100

    fix Category table header display name

--- Storage/Models/Category.cs
+++ Storage/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace Storage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Display(Name = "Category")]
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public IEnumerable<Product>? Products { get; set; }
